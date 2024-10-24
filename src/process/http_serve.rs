20e2331b9d8d05c8ba525997c2da8a74c1427152
liuzm <liuzm920520@gmail.com>
2024-10-24 21:46:21 +0800

    http serve

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -33,15 +33,43 @@ async fn file_handler(
     if !p.exists() {
         (StatusCode::NOT_FOUND, format!("File {} not found!", p.display()))
     } else {
-        match tokio::fs::read_to_string(p).await {
-            Ok(content) => {
-                info!("Read {} bytes", content.len());
-                (StatusCode::OK, content)
-            },
-            Err(err) => {
-                warn!("Error reading file {}", err);
-                (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
-            },
+        if p.is_dir() {
+            let out_path = p.clone().join("index.html");
+            // 如果是目录，返回目录内容列表 
+            // 目录下所有文件输出到新建的index.html文件中， 格式 <html><body><ul>...</ul></body></html>
+            // 使用 tokio 读取目录下所有文件
+            let mut files: Vec<String> = Vec::new();
+            let mut entries = tokio::fs::read_dir(p).await.unwrap();
+
+            while let Some(entry) = entries.next_entry().await.unwrap() {
+                let path = entry.path();
+                let file_name = path.file_name().unwrap().to_str().unwrap();
+                files.push(file_name.to_string());
+                
+            }
+            // 按照文件名排序， 每一条格式 <li><a href="path">path</a></li>
+            files.sort();
+            let mut html = String::new();
+            html.push_str("<html><body><ul>");
+            for file in files {
+                html.push_str(&format!("<li><a href=\"{}\">{}</a></li>", file, file));
+            }
+            html.push_str("</ul></body></html>");
+            // html 中的内容 渲染返回
+            tokio::fs::write(out_path, html).await.unwrap();
+            (StatusCode::OK, "None".to_string())
+        } else {
+            // 如果文件存在，读取文件内容并返回
+            match tokio::fs::read_to_string(p).await {
+                Ok(content) => {
+                    info!("Read {} bytes", content.len());
+                    (StatusCode::OK, content)
+                },
+                Err(err) => {
+                    warn!("Error reading file {}", err);
+                    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+                },
+            }
         }
     }
 }
@@ -54,7 +82,7 @@ mod tests {
     #[tokio::test]
     async fn test_file_handler() {
         let state = HttpServeState { path: PathBuf::from(".") };
-        let (status, content) = file_handler(State(Arc::new(state)), Path("Cargo.toml".to_string())).await;
+        let (status, content) = file_handler(State(Arc::new(state)), Path("fixtures".to_string())).await;
         assert_eq!(status, StatusCode::OK);
         assert!(content.contains("[package]"));
 
